59ee947229d6d99da49c15dbe6864ee9fe7db022
NgadiHIQ <ngadiabdelquayoum@gmail.com>
2023-10-11 17:24:36 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,10 +1,8 @@
 #include "main.h"
 /**
- * print_sign - prints the sign of a number
- * @n: the int to check
- * Return: 1 and prints + if n is greater than zero
- * 0 and prints 0 if n is zero
- * -1 and prints - if n is less than zero
+ * print_sign - prints sign of a number
+ * @n: function parameter
+ * Return: 1, 0 or -1
  */
 int print_sign(int n)
 {
